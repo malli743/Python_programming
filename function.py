fa81906c25326c577e47e80bfe199066a80c30ef
Potla Mallikharjuna Rao <mallikharjunapotla@gmail.com>
2026-08-24 12:44:45 +0530

    Add files via upload

---
+++ function.py
@@ -0,0 +1,11 @@
+#def main():
+# print("Hello,world")
+# print("this is cs43")
+#main()
+def greet(input):
+    if "hello" in input:
+        return "hello,there"
+    else:
+        return "I'm not sure wht you mean"
+greeting = greet("hai,this is mallikharjun")
+print(greeting)
